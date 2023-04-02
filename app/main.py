e440ff67eaaa09fda7d1f9e2f85b88372f4a0081
erez <4206293+zeako@users.noreply.github.com>
2023-04-02 15:18:42 +0300

    minimum functionality

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from ipaddress import IPv4Address
+import time
 from typing import Any, Optional
 
 import uvicorn
@@ -8,18 +9,23 @@ from fastapi_cache.backends.inmemory import InMemoryBackend
 from fastapi_cache.decorator import cache
 from pydantic import BaseModel
 
-from app.httpclient import HTTPXClientWrapper
+from app.httpclient.httpclient import HTTPXClientWrapper
+
+
+class Metric(BaseModel):
+    status: str # can use enum
+    time: int
 
 
 class Metrics(BaseModel):
-    ip_api: dict[str, Any]
-    bgview: dict[str, Any]
-    total: dict[str, Any]
+    ip_api: Metric
+    bgview: Metric
+    total: Metric
 
 
 class RawData(BaseModel):
     ip_api: Optional[dict[str, Any]]
-    bgview: Optional[dict[str, Any]]
+    bgpview: Optional[dict[str, Any]]
 
 
 class IpAnalysisResponse(BaseModel):
@@ -34,7 +40,42 @@ app = FastAPI()
 @app.get('/{ip_address}')
 @cache(expire=3600)
 async def fetch_ip_analysis(ip_address: IPv4Address) -> IpAnalysisResponse:
-    pass
+    async_client = http_client()
+
+    start_time = time.monotonic()
+
+    ip_api_start = start_time
+    ip_api_res = await async_client.get(f'http://ip-api.com/json/{ip_address}')
+    ip_api_res_end = time.monotonic() - ip_api_start
+
+
+    bgpview_start = time.monotonic()
+    bgpview_res = await async_client.get(f'https://api.bgpview.io/ip/{ip_address}')
+    bgpview_res_end = time.monotonic() - bgpview_start
+
+    total_time = time.monotonic() - start_time
+
+    metrics = Metrics(
+        ip_api=Metric(
+            status='success' if ip_api_res.is_success else 'failure',
+            time=ip_api_res_end
+        ),
+        bgview=Metric(
+            status='success' if bgpview_res.is_success else 'failure',
+            time=bgpview_res_end
+        ),
+        total=Metric(
+            status='success' if ip_api_res.is_success and bgpview_res.is_success else 'failure',
+            time=total_time
+        )
+    )
+
+    return IpAnalysisResponse(
+        metrics=metrics,
+        raw_data=RawData(
+            ip_api=ip_api_res.json(),
+            bgpview=bgpview_res.json()
+        ))
 
 
 @app.on_event('startup')
